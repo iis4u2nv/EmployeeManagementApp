a04bed7b491c121be307c2f38f15e7ebc0ba3f1f
amazing grace <agracenotary@gmail.com>
2023-07-11 18:30:37 -0400

    modified content

--- index.js
+++ index.js
@@ -175,11 +175,3 @@ function updateEmployee() {
 })
 }
 menu();
-
-// .then((answers) => {
-//   const htmlPageContent = generateHTML(answers);
-
-//   fs.writeFile('index.html', htmlPageContent, (err) =>
-//     err ? console.log(err) : console.log('Successfully created index.html!')
-//   );
-// });
